14334b30f4f923360a9255e0ae4b5c300b929312
Wisnu Hafid <inuvalogic@gmail.com>
2017-11-20 14:55:59 +0700

    constant

---
+++ lat-3.php
@@ -0,0 +1,14 @@
+<?php
+
+$angka = 100;
+$angka = $angka*2;
+
+define('NAMA_CONSTANT', $angka);
+define('HURUF_BESAR', 'ini nilai untuk HURUF BESAR', true); // incase sensitive, kalau true bisa huruf besar atau kecil, defaultnya false
+
+define('__DIR__', 'halo'); // predefined constant tidak bisa di set seperti ini
+
+echo NAMA_CONSTANT . '<br>';
+echo __DIR__ . '<br>'; // predefined constant
+echo huruf_besar;
+
