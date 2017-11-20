c18a76d39c86816ab17ffb31e3904838bc5f33ea
Wisnu Hafid <inuvalogic@gmail.com>
2017-11-20 14:15:06 +0700

    basic syntax

---
+++ lat-1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan PHP</title>
+</head>
+<body>
+
+<?php
+
+// ini komentar (teks yang tidak akan tampil di browser)
+
+# ini ga bakal muncul
+
+/*
+ini untuk lebih dari satu baris
+*/
+
+echo "Hello World<br>ini baris baru\nbaris baru lagi\n";
+
+echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n\n\n\n
+Adipisci architecto dolores delectus nobis dolorem ducimus ea maxime, corrupti voluptate, 
+cumque rerum vel ab animi quam voluptatibus reiciendis possimus.
+Laborum, reprehenderit.";
+
+echo ((5 + 2 ) * 3 ) / 7;
+echo 21 % 2;
+
+?>
+
+</body>
+</html>
